cbcb8dac3706bc895dab895e0597c571257d85a1
Marc Tushman <marctushman@Marcs-MacBook-Pro-3.local>
2023-12-06 18:09:43 -0500

    grading rubric changes

--- SI206finalbaseball.py
+++ SI206finalbaseball.py
@@ -37,6 +37,10 @@ def create_tables(cur, conn):
     conn.commit()
     cur.execute('CREATE TABLE IF NOT EXISTS baseball_stats (player_id INTEGER PRIMARY KEY, player_avg NUMERIC, player_ops NUMERIC, player_obp NUMERIC)')
     conn.commit()
+    cur.execute('CREATE TABLE IF NOT EXISTS baseball_25_rows_4_times (player_id INTEGER PRIMARY KEY, player_name TEXT, team_id INTEGER, team_name TEXT)')
+    conn.commit()
+    # cur.execute('DROP TABLE IF EXISTS baseball_25_rows_4_times')
+    # conn.commit()
 
 
 def add_team(): 
@@ -53,6 +57,55 @@ def add_team():
             if count%25 == 0:
                 time.sleep(5)
         return(team_id_lst)
+    
+def show_25_rows(cur, conn):
+    player_id_lst = []
+    team_id_list = add_team()
+    inserted_count = 0  # To keep track of the number of inserted rows
+
+    # Fetch the current count from the database
+    cur.execute('SELECT COUNT(*) FROM baseball_25_rows_4_times')
+    count_result = cur.fetchone()
+    current_count = count_result[0] if count_result else 0
+    # print(current_count)
+    if current_count==25:
+        team_id_list.pop(0)
+    elif current_count==50:
+        team_id_list.pop(0)
+        team_id_list.pop(0)
+    elif current_count==75:
+        team_id_list.pop(0)
+        team_id_list.pop(0)
+        team_id_list.pop(0)
+    elif current_count>=100:
+        return 'Already added 25 items 4 times'
+
+    for team in team_id_list:
+        if inserted_count >= 25:
+            break  # Stop if 25 rows have already been inserted
+
+        url = f"http://lookup-service-prod.mlb.com/json/named.roster_40.bam?team_id='{team}'"
+        data = get_baseball_info(url)
+
+        for player in data['roster_40']['queryResults']['row']:
+
+            
+            
+            if inserted_count >= 25:
+                break  # Stop if 25 rows have already been inserted
+
+            player_id = player['player_id']
+            player_name = player['name_display_first_last']
+            team_id = player['team_id']
+            team_name = player['team_name']
+            player_id_lst.append(player_id)
+
+            # Insert data into the database
+            cur.execute('INSERT OR IGNORE INTO baseball_25_rows_4_times (player_id, player_name, team_id, team_name) VALUES(?, ?, ?, ?)', (player_id, player_name, team_id, team_name))
+            conn.commit()
+            inserted_count += 1
+
+    return player_id_lst
         
 
 def add_player(cur, conn):
@@ -227,6 +280,7 @@ def calculate_stats_baseball(filename, cur, conn):
 def main():
     cur, conn = setUpDatabase('sport_analysis.db')
     create_tables(cur, conn)
+    show_25_rows(cur, conn)
     add_player(cur,conn)
     add_stats(cur,conn)
     calculate_stats_baseball("baseball_data.json", cur,conn)
